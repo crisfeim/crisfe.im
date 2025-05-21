0c1f62e6e8f445d5d0a86474dcbf325a9c10704e
Cristian Rojas <cristian.rojas@live.fr>
2025-05-21 20:42:24 +0200

    Move gemini client to its own file

---
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/geminiclient.e2e.test.ts
@@ -0,0 +1,16 @@
+import { Message } from "./coordinator.ts";
+import { assertStringIncludes } from "https://deno.land/std/assert/mod.ts";
+import { GeminiClient } from "./geminiclient.ts";
+import { gemini_api_key } from "./secret_api_keys.ts";
+
+Deno.test("GeminiClient: send", async () => {
+  const client = new GeminiClient(gemini_api_key);
+  const messages: Message[] = [
+    { role: "system", content: "You always respond with a single word: hi" },
+    { role: "user", content: "Hello" },
+  ];
+
+  const response = await client.send(messages);
+
+  assertStringIncludes(response.toLocaleLowerCase(), "hi");
+});
