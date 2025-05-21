7d41d81579954cf74f01c214361c742373990b88
Cristian Rojas <cristian.rojas@live.fr>
2025-05-21 21:49:36 +0200

    EvalRunner assertEqual doesn't throw on equal values

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/tests/evalrunner.test.ts
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/tests/evalrunner.test.ts
@@ -35,3 +35,24 @@ Deno.test("EvalRunner assertEqual throws on different values", () => {
   assertEquals(result.stdErr, "Error: Assertion failed: expected 4, but got 3")
   assert(result.isValid === false);
 });
+
+Deno.test("EvalRunner assertEqual doesn't throw on equal values", () => {
+  const runner = new EvalRunner();
+  const code = `
+    class Adder {
+      constructor(a, b) {
+        this.result = a + b;
+      }
+    }
+
+    function testAdder() {
+      const sut = new Adder(1, 2);
+      assertEqual(sut.result, 3);
+    }
+
+    testAdder();
+  `
+  const result = runner.run(code);
+  assert(result.isValid === true);
+  assert(result.stdErr === undefined)
+});
