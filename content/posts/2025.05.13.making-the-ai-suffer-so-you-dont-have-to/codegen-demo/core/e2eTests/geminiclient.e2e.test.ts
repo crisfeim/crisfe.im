dfda6281101358c376747a4d8630689c098cadca
Cristian Rojas <cristian.rojas@live.fr>
2025-05-21 21:01:01 +0200

    Move e2eTests to their own folder

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/e2eTests/geminiclient.e2e.test.ts
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/e2eTests/geminiclient.e2e.test.ts
@@ -1,6 +1,6 @@
-import { Message } from "./coordinator.ts";
+import { Message } from "../coordinator.ts";
 import { assertStringIncludes } from "https://deno.land/std/assert/mod.ts";
-import { GeminiClient } from "./geminiclient.ts";
+import { GeminiClient } from "../geminiclient.ts";
 import { gemini_api_key } from "./secret_api_keys.ts";
 
 Deno.test("GeminiClient: send", async () => {
