2056e54fcd1f0b9d639c5cb599e7d40789ef0643
Cristian Rojas <cristian.rojas@live.fr>
2025-05-21 17:17:18 +0200

    generateAndEvaluateCode delivers error on runner error

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/generator.test.ts
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/generator.test.ts
@@ -4,6 +4,10 @@ interface Client {
    generateCode(): Promise<string>;
 }
 
+interface Runner {
+  run(code: string): void
+}
+
 class ClientStub implements Client {
   constructor(private result: string | Error) {}
   async generateCode(): Promise<string> {
@@ -14,26 +18,46 @@ class ClientStub implements Client {
   }
 }
 
+class RunnerStub implements Runner {
+  constructor(private result: void | Error) {}
+  run(code: string): void {
+    if (this.result instanceof Error) {
+      throw this.result
+    }
+  }
+}
+
 class Coordinator {
-  constructor(private client: Client) {
+  constructor(private client: Client, private runner: Runner) {
     this.client = client
+    this.runner = runner
   }
 
   async generateAndEvaluateCode(): Promise<string> {
-    return await this.client.generateCode()
+    const generated = await this.client.generateCode()
+    this.runner.run(generated)
+    return generated
   }
 }
 
 Deno.test("generateAndEvaluateCode delivers error on client error", async () => {
   const anyError = Error("any error")
   const client = new ClientStub(anyError)
-  const sut = new Coordinator(client)
+  const sut = new Coordinator(client, new RunnerStub())
   await assertRejects(()=>sut.generateAndEvaluateCode(), Error, "any error")
 });
 
 Deno.test("generateAndEvaluateCode delivers code on client succes", async () => {
   const client = new ClientStub("any code")
-  const sut = new Coordinator(client)
+  const sut = new Coordinator(client, new RunnerStub())
   const result = await sut.generateAndEvaluateCode()
   assertEquals(result, "any code")
+})
+
+Deno.test("generateAndEvaluateCode delivers error on runner error", async () => {
+  const anyError = Error("any error")
+  const client = new ClientStub("any code")
+  const runner = new RunnerStub(anyError)
+  const sut = new Coordinator(client, runner)
+  await assertRejects(()=>sut.generateAndEvaluateCode(), Error, "any error")
 })
