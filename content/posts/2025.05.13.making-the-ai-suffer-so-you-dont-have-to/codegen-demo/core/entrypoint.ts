4965ec91c0cbdd6850cc86911b1cfafe4e6c40bd
Cristian Rojas <cristian.rojas@live.fr>
2025-05-22 11:51:51 +0200

    Move factories to entry point and use in integration tests

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/entrypoint.ts
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/codegen-demo/core/entrypoint.ts
@@ -1,16 +1,4 @@
-import { makeReactiveViewModel , AppState, Status} from "./viewModel.ts";
+import { ollamaViewModel, geminiViewModel } from "./_entrypoint";
 
-import { OllamaClient } from "./ollamaclient.ts"
-import { EvalRunner } from "./evalrunner.ts"
-
-const defaultVM: () => AppState & {
-  status: () => Status | undefined;
-  generatedCode: () => string | undefined;
-  run: () => Promise<void>;
-} = () => {
-  const client = new OllamaClient();
-  const runner = new EvalRunner();
-  return makeReactiveViewModel(client, runner);
-};
-
-this.defaultVM = defaultVM;
+this.ollamaViewModel = ollamaViewModel;
+this.geminiViewModel = geminiViewModel;
