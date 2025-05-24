f6db5442b95b50a1c0be92b0e69bd0b2875a3f59
Cristian Rojas <cristian.rojas@live.fr>
2025-05-24 09:07:32 +0200

    Export llm7viewModle

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/demo/core/entrypoint.ts
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/demo/core/entrypoint.ts
@@ -1,5 +1,6 @@
-import { ollamaViewModel, geminiViewModel, fakeClientViewModel } from "./_entrypoint";
+import { ollamaViewModel, geminiViewModel, fakeClientViewModel, llm7ViewModel } from "./_entrypoint";
 
 this.ollamaViewModel = ollamaViewModel;
 this.geminiViewModel = geminiViewModel;
 this.fakeClientViewModel = fakeClientViewModel;
+this.llm7ViewModel = llm7ViewModel;
