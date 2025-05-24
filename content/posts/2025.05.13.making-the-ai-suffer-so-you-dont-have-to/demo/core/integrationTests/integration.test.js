a483665c7620c03ff2d9dd67aa35470eed4792a8
Cristian Rojas <cristian.rojas@live.fr>
2025-05-24 09:09:07 +0200

    Test success on integration test and bump max iteration count. We want to test that models provide correct solution and problem is simple enough to be almost deterministic with a high iteration count.

--- content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/demo/core/integrationTests/integration.test.js
+++ content/posts/2025.05.13.making-the-ai-suffer-so-you-dont-have-to/demo/core/integrationTests/integration.test.js
@@ -1,33 +1,25 @@
-import { assertExists, assertNotEquals } from "https://deno.land/std/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std/assert/mod.ts";
 import { EvalRunner } from "../evalrunner.ts"
 import { ollamaViewModel, geminiViewModel, llm7ViewModel } from "../_entrypoint.ts";
 
 
-const maxIterations = 1
+const maxIterations = 15
+
 Deno.test("Ollama client integration test", async () => {
   const sut = ollamaViewModel(maxIterations)
   await sut.run()
-
-  assertExists(sut.generatedCode)
-  assertExists(sut.status)
-  assertNotEquals(sut.currentIteration, 0)
+  assertEquals(sut.status, 'success')
 })
 
 import { gemini_api_key } from "../secret_api_keys.ts"
 Deno.test("Geminii client integration test", async () => {
   const sut = geminiViewModel(gemini_api_key, maxIterations)
   await sut.run()
-
-  assertExists(sut.generatedCode)
-  assertExists(sut.status)
-  assertNotEquals(sut.currentIteration, 0)
+  assertEquals(sut.status, 'success')
 })
 
 Deno.test("LLM7 client integration test", async () => {
   const sut = llm7ViewModel(maxIterations)
   await sut.run()
-
-  assertExists(sut.generatedCode)
-  assertExists(sut.status)
-  assertNotEquals(sut.currentIteration, 0)
+  assertEquals(sut.status, 'success')
 })
