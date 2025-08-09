0ec6149635e996a70a90cd325040606483446f2d
Cristian Rojas <cristian.rojas@live.fr>
2025-08-10 00:27:30 +0200

    Remove redundant code on tabbar.swift

--- content/public-drafts/when-abstractions-are-worth-it/snippets/tabbar.swift
+++ content/public-drafts/when-abstractions-are-worth-it/snippets/tabbar.swift
@@ -24,18 +24,3 @@ struct Tabbar: View {
         }
     }
 }
-
-protocol RecipesLoader {
-    func execute() async throws -> [Recipe]
-}
-
-struct RecipeList: View {
-    @State var recipes = [Recipe]()
-    let loader: RecipesLoader
-    var body: some View {
-        List(recipes) { Text($0.title) }
-            .task {
-               recipes = try await loader.execute()
-            }
-    }
-}
